eacec3622bdfe2574558bffbe1fbf982d4e35b79
Julia Waggoner <jjwaggoner27@gmail.com>
2016-10-24 23:26:56 -0400

    connect company to json

--- SeniorSeminar/CompanyViewController.swift
+++ SeniorSeminar/CompanyViewController.swift
@@ -12,11 +12,40 @@ class CompanyViewController: UIViewController, UITableViewDelegate, UITableViewD
 
     @IBOutlet weak var table2: UITableView!
     
+     var companyOfMifi = [Company]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         table2.delegate = self
         table2.dataSource = self
+        
+        parseJSON()
     }
+    
+    ////////////////
+    //parse the main JSON for the main view controller
+    
+    func parseJSON(){
+        do{
+            let data = NSData(contentsOfURL: NSURL(string: "https://jsonblob.com/api/jsonBlob/580d0ccce4b0bcac9f837fbe")!)
+            
+            let jsonResult = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)
+            
+            for anItem in jsonResult as! [Dictionary<String, AnyObject>]{
+                
+                let companyName2 = anItem["company"] as! String
+                
+                let newCompany = Company(companyName: companyName2)
+                companyOfMifi.append(newCompany)
+                
+            }
+        }
+        catch let error as NSError{
+            print(error.debugDescription)
+        }
+    }
+    
+    /////////////////////////
     
     @IBAction func btnPressed(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
@@ -27,6 +56,9 @@ class CompanyViewController: UIViewController, UITableViewDelegate, UITableViewD
         //instead of creating new cells it just reuses old cells
         if let cell = tableView.dequeueReusableCellWithIdentifier("CompanyCell", forIndexPath: indexPath)as? CompanyCell{
             
+            let company: Company!
+            company = companyOfMifi[indexPath.row]
+            cell.configureCell(company)
             return cell
             
         } else{
@@ -40,7 +72,7 @@ class CompanyViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 30
+        return companyOfMifi.count
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
